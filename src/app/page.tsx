55aeadef2d3424a2461c099f28e8820fa7e6fe2e
Thelucasdias <llucasdias@yahoo.com.br>
2025-10-01 19:20:27 -0300

    staleTime fixed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,7 +9,10 @@ export default function Page() {
     data: btcPrice,
     isLoading,
     error,
-  } = trpc.public.getBtcPrice.useQuery();
+  } = trpc.public.getBtcPrice.useQuery(undefined, {
+    staleTime: 300_000,
+    refetchOnWindowFocus: false,
+  });
 
   return (
     <>
